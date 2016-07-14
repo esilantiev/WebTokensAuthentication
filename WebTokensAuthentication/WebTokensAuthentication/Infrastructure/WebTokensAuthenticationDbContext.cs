56bf9d5c18ec20013ec614f0f1a0b6caae1b99fe
esilantiev <silantiev.evgheni@gmail.com>
2016-07-14 17:14:46 +0300

    Configured Asp.Net Identity and WebApi

---
+++ WebTokensAuthentication/WebTokensAuthentication/Infrastructure/WebTokensAuthenticationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebTokensAuthentication.Models;
+
+namespace WebTokensAuthentication.Infrastructure
+{
+    public class WebTokensAuthenticationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public WebTokensAuthenticationDbContext()
+            : base("WebTokensAuthenticationDbContext", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static WebTokensAuthenticationDbContext Create()
+        {
+            return new WebTokensAuthenticationDbContext();
+        }
+    }
+}
